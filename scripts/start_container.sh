915b148535c5b336fa7ef59a5455c0573ae9b216
Omkar Khilare <127951525+om-25@users.noreply.github.com>
2023-10-20 22:31:31 +0530

    add the files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull om25/simple-python-flask-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000  docker pull om25/simple-python-flask-app
+
+
+
+
+
+
