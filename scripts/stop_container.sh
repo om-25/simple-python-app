7c6385c55120406c5b485f107fae01c01e9a89f1
Omkar Khilare <127951525+om-25@users.noreply.github.com>
2023-10-21 10:11:17 +0530

    edit

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-containerId='docker ps | awk -F " " '{print$1}''
-docker rm -f $containerId
+# containerId='docker ps | awk -F " " '{print $1}''
+# docker rm -f $containerId
 
 
 
